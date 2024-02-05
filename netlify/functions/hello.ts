5f34ac40de55fdc78a94174dfe77a9a7fd517ff9
jrwhitmer <jacqwhitmer@gmail.com>
2024-02-05 14:20:15 -0800

    make it REAL BAD

--- netlify/functions/hello.ts
+++
@@ -1,9 +0,0 @@
-import { Config, Context } from "@netlify/functions"
-
-export default async (req: Request, context: Context) => {
-  return new Response(`Hello Functions`)
-}
-
-export const config: Config = {
-  path: ["/custom/functions", "/custom/functions/2"]
-}
